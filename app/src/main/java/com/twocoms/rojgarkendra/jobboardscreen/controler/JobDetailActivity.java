5bec1a5440575373a2aaed9dd355ce4a53ab5fa1
satyam.vishwakarma <satyam.vishwakarma@ril.comn>
2021-03-22 22:43:15 +0530

    Job detail added

--- app/src/main/java/com/twocoms/rojgarkendra/jobboardscreen/controler/JobDetailActivity.java
+++ app/src/main/java/com/twocoms/rojgarkendra/jobboardscreen/controler/JobDetailActivity.java
@@ -2,7 +2,9 @@ package com.twocoms.rojgarkendra.jobboardscreen.controler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
@@ -11,16 +13,20 @@ import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.twocoms.rojgarkendra.R;
+import com.twocoms.rojgarkendra.databinding.ActivityJobDetailBinding;
 import com.twocoms.rojgarkendra.global.model.AppConstant;
 import com.twocoms.rojgarkendra.global.model.CommonMethod;
 import com.twocoms.rojgarkendra.global.model.ServiceHandler;
 import com.twocoms.rojgarkendra.jobboardscreen.model.ModelHotJobs;
+import com.twocoms.rojgarkendra.jobboardscreen.model.VacancyDetailModel;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JobDetailActivity extends AppCompatActivity {
+
+    private ActivityJobDetailBinding jobDetailBinding;
 
     ImageView menuIcon, backIcon, homeIcon, profileIcon;
     TextView titleToolbar;
@@ -30,52 +36,79 @@ public class JobDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_job_detail);
+        jobDetailBinding = ActivityJobDetailBinding.inflate(getLayoutInflater());
+        View view = jobDetailBinding.getRoot();
+        setContentView(view);
         initialization();
         setToolbarVisibility();
         onClick();
     }
 
     void initialization() {
-        menuIcon = (ImageView) findViewById(R.id.menu);
-        backIcon = (ImageView) findViewById(R.id.backbutton);
-        homeIcon = (ImageView) findViewById(R.id.home_img);
-        profileIcon = (ImageView) findViewById(R.id.user_profile_img);
-        titleToolbar = (TextView) findViewById(R.id.title);
-        titleLnr = (LinearLayout) findViewById(R.id.title_lnr);
-        titleToolbar.setText("Vacancy Detail");
+//        jobDetailBinding.successStoriesToolbar.menu.setVisibility(View.GONE);
+//        menuIcon = (ImageView) findViewById(R.id.menu);
+//        backIcon = (ImageView) findViewById(R.id.backbutton);
+//        homeIcon = (ImageView) findViewById(R.id.home_img);
+//        profileIcon = (ImageView) findViewById(R.id.user_profile_img);
+//        titleToolbar = (TextView) findViewById(R.id.title);
+//        titleLnr = (LinearLayout) findViewById(R.id.title_lnr);
+        jobDetailBinding.successStoriesToolbar.title.setText("Vacancy Detail");
         jobId = getIntent().getStringExtra("jobId");
         getJobDetail();
     }
 
     void setToolbarVisibility() {
-        menuIcon.setVisibility(View.GONE);
-        backIcon.setVisibility(View.VISIBLE);
-        homeIcon.setVisibility(View.VISIBLE);
-        profileIcon.setVisibility(View.GONE);
+        jobDetailBinding.successStoriesToolbar.menu.setVisibility(View.GONE);
+        jobDetailBinding.successStoriesToolbar.userProfileImg.setVisibility(View.GONE);
+        jobDetailBinding.successStoriesToolbar.backbutton.setVisibility(View.VISIBLE);
+        jobDetailBinding.successStoriesToolbar.homeImg.setVisibility(View.VISIBLE);
+//        menuIcon.setVisibility(View.GONE);
+//        backIcon.setVisibility(View.VISIBLE);
+//        homeIcon.setVisibility(View.VISIBLE);
+//        profileIcon.setVisibility(View.GONE);
     }
 
     void onClick() {
-        backIcon.setOnClickListener(new View.OnClickListener() {
+
+        jobDetailBinding.successStoriesToolbar.backbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        jobDetailBinding.successStoriesToolbar.homeImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
+        jobDetailBinding.successStoriesToolbar.title.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 onBackPressed();
             }
         });
 
-        homeIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
-
-        titleLnr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                onBackPressed();
-            }
-        });
+//        backIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                onBackPressed();
+//            }
+//        });
+//
+//        homeIcon.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                onBackPressed();
+//            }
+//        });
+//
+//        titleLnr.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                onBackPressed();
+//            }
+//        });
     }
 
 
@@ -90,10 +123,66 @@ public class JobDetailActivity extends AppCompatActivity {
                     JSONObject jsonObject = new JSONObject(result);
                     String message = jsonObject.getString("message");
                     if (jsonObject.getBoolean("success")) {
-
-                        JSONObject object = jsonObject.getJSONObject("data");
-                        JSONArray jsonArray = object.getJSONArray("data");
-
+                        JSONObject jsonObjectData = jsonObject.getJSONObject("data");
+                        VacancyDetailModel vacancyDetailModel = new VacancyDetailModel();
+                        vacancyDetailModel.setId(jsonObjectData.getInt("id"));
+                        vacancyDetailModel.setVacancy_master_id(jsonObjectData.getString("vacancy_master_id"));
+                        vacancyDetailModel.setNaps_opportunity_id(jsonObjectData.getString("naps_opportunity_id"));
+                        vacancyDetailModel.setZoho_recruit_id(jsonObjectData.getString("zoho_recruit_id"));
+                        vacancyDetailModel.setZoho_sourcing_id(jsonObjectData.getString("zoho_sourcing_id"));
+                        vacancyDetailModel.setVacancy_title(jsonObjectData.getString("vacancy_title"));
+                        vacancyDetailModel.setClient_name(jsonObjectData.getString("client_name"));
+                        vacancyDetailModel.setIndustry(jsonObjectData.getInt("industry"));
+                        vacancyDetailModel.setContact_name(jsonObjectData.getString("contact_name"));
+                        vacancyDetailModel.setNumber_of_open_positions(jsonObjectData.getInt("number_of_open_positions"));
+                        vacancyDetailModel.setYears_of_exp_required(jsonObjectData.getInt("years_of_exp_required"));
+                        vacancyDetailModel.setMonths_of_exp_required(jsonObjectData.getInt("months_of_exp_required"));
+                        vacancyDetailModel.setJob_responsibilities(jsonObjectData.getString("job_responsibilities"));
+                        vacancyDetailModel.setGender_preferences(jsonObjectData.getString("gender_preferences"));
+                        vacancyDetailModel.setJob_description(jsonObjectData.getString("job_description"));
+                        vacancyDetailModel.setSkills(jsonObjectData.getString("skills"));
+                        vacancyDetailModel.setLanguage_preference(jsonObjectData.getString("language_preference"));
+                        vacancyDetailModel.setMinimum_education(jsonObjectData.getString("minimum_education"));
+                        vacancyDetailModel.setOther_eligibility_criterea(jsonObjectData.getString("other_eligibility_criterea"));
+                        vacancyDetailModel.setHeight(jsonObjectData.getInt("height"));
+                        vacancyDetailModel.setWeight(jsonObjectData.getInt("weight"));
+                        vacancyDetailModel.setShift_type(jsonObjectData.getString("shift_type"));
+                        vacancyDetailModel.setShift_timing(jsonObjectData.getString("shift_timing"));
+                        vacancyDetailModel.setFooding(jsonObjectData.getString("fooding"));
+                        vacancyDetailModel.setLodging(jsonObjectData.getString("lodging"));
+                        vacancyDetailModel.setOther_benefits_for_employees(jsonObjectData.getString("other_benefits_for_employees"));
+                        vacancyDetailModel.setIn_hand_salary(jsonObjectData.getString("in_hand_salary"));
+                        vacancyDetailModel.setCtc(jsonObjectData.getString("ctc"));
+                        vacancyDetailModel.setCurrency(jsonObjectData.getString("currency"));
+                        vacancyDetailModel.setMinimum_salary_stipend(jsonObjectData.getString("minimum_salary_stipend"));
+                        vacancyDetailModel.setMaximum_salary_stipend(jsonObjectData.getString("maximum_salary_stipend"));
+                        vacancyDetailModel.setOvertime_per_day_hr(jsonObjectData.getString("overtime_per_day_hr"));
+                        vacancyDetailModel.setOt_amount(jsonObjectData.getString("ot_amount"));
+                        vacancyDetailModel.setJob_opening_status(jsonObjectData.getString("job_opening_status"));
+                        vacancyDetailModel.setDate_opened(jsonObjectData.getString("date_opened"));
+                        vacancyDetailModel.setJob_type(jsonObjectData.getString("job_type"));
+                        vacancyDetailModel.setIs_hot_job_opening(jsonObjectData.getString("is_hot_job_opening"));
+                        vacancyDetailModel.setAssigned_recruiter(jsonObjectData.getString("assigned_recruiter"));
+                        vacancyDetailModel.setCity(jsonObjectData.getString("city"));
+                        vacancyDetailModel.setState_province(jsonObjectData.getString("state_province"));
+                        vacancyDetailModel.setCountry(jsonObjectData.getString("country"));
+                        vacancyDetailModel.setPostal_code(jsonObjectData.getInt("postal_code"));
+                        vacancyDetailModel.setRegion_of_work_location_in_india(jsonObjectData.getString("region_of_work_location_in_india"));
+                        vacancyDetailModel.setAdded_by(jsonObjectData.getDouble("added_by"));
+                        vacancyDetailModel.setCreated_on(jsonObjectData.getString("created_on"));
+                        vacancyDetailModel.setEdited_by(jsonObjectData.getDouble("edited_by"));
+                        vacancyDetailModel.setUpdated_on(jsonObjectData.getString("updated_on"));
+                        vacancyDetailModel.setPublished(jsonObjectData.getString("published"));
+                        vacancyDetailModel.setRevenue_per_position(jsonObjectData.getString("revenue_per_position"));
+                        vacancyDetailModel.setIndsId(jsonObjectData.getDouble("indsId"));
+                        vacancyDetailModel.setInds_name(jsonObjectData.getString("inds_name"));
+                        vacancyDetailModel.setInds_desc(jsonObjectData.getString("inds_desc"));
+                        vacancyDetailModel.setInds_logo(jsonObjectData.getString("inds_logo"));
+                        vacancyDetailModel.setInds_status(jsonObjectData.getString("inds_status"));
+                        vacancyDetailModel.setCreated_on(jsonObjectData.getString("created_at"));
+                        vacancyDetailModel.setStCode(jsonObjectData.getDouble("StCode"));
+                        vacancyDetailModel.setStateName(jsonObjectData.getString("StateName"));
+                        setDataonView(vacancyDetailModel);
 
                     } else {
                         CommonMethod.showToast(message, JobDetailActivity.this);
@@ -107,6 +196,39 @@ public class JobDetailActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+
+    void setDataonView(VacancyDetailModel vacancyDetailModel) {
+        Log.e("vacancyDetailModel", "fecthed");
+        jobDetailBinding.salaryText.setText(getResources().getString(R.string.Rs) + CommonMethod.roundNumbertoNextPossibleValue(vacancyDetailModel.getCtc()));
+        jobDetailBinding.clientText.setText(vacancyDetailModel.getClient_name());
+        jobDetailBinding.jobTypeText.setText(vacancyDetailModel.getJob_type());
+        jobDetailBinding.locationText.setText(vacancyDetailModel.getCity());
+        jobDetailBinding.vacancyText.setText(CommonMethod.roundNumbertoNextPossibleValue(vacancyDetailModel.getNumber_of_open_positions()+"") + " open position");
+        jobDetailBinding.jobSkills.setText(vacancyDetailModel.getSkills());
+        if (vacancyDetailModel.getGender_preferences().equalsIgnoreCase("male")) {
+            jobDetailBinding.jobPreferredGender.setText("Male");
+        } else {
+            jobDetailBinding.jobPreferredGender.setText("Female");
+        }
+        jobDetailBinding.jobExperinaceRequired.setText(CommonMethod.roundNumbertoNextPossibleValue(vacancyDetailModel.getYears_of_exp_required() + "") + " Years");
+        jobDetailBinding.jobMinimumQualificationRequired.setText(vacancyDetailModel.getMinimum_education());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            jobDetailBinding.jobDesciption.setText(Html.fromHtml(vacancyDetailModel.getJob_description(), Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            jobDetailBinding.jobDesciption.setText(Html.fromHtml(vacancyDetailModel.getJob_description()));
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            jobDetailBinding.jobResponsiblity.setText(Html.fromHtml(vacancyDetailModel.getJob_responsibilities(), Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            jobDetailBinding.jobResponsiblity.setText(Html.fromHtml(vacancyDetailModel.getJob_responsibilities()));
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            jobDetailBinding.jobBenefit.setText(Html.fromHtml(vacancyDetailModel.getOther_benefits_for_employees(), Html.FROM_HTML_MODE_COMPACT));
+        } else {
+            jobDetailBinding.jobBenefit.setText(Html.fromHtml(vacancyDetailModel.getOther_benefits_for_employees()));
+        }
     }
 
 
---
+++ app/src/main/java/com/twocoms/rojgarkendra/jobboardscreen/model/VacancyDetailModel.java
@@ -0,0 +1,526 @@
+package com.twocoms.rojgarkendra.jobboardscreen.model;
+
+public class VacancyDetailModel {
+
+    public VacancyDetailModel() {
+    }
+
+    private int id;
+    private String vacancy_master_id;
+    private String naps_opportunity_id;
+    private String zoho_recruit_id;
+    private String zoho_sourcing_id;
+    private String vacancy_title;
+    private String client_name;
+    private double industry;
+    private String contact_name;
+    private double number_of_open_positions;
+    private double years_of_exp_required;
+    private double months_of_exp_required;
+    private String job_responsibilities;
+    private String gender_preferences;
+    private String job_description;
+    private String skills;
+    private String language_preference = null;
+    private String minimum_education;
+    private String other_eligibility_criterea;
+    private double height;
+    private double weight;
+    private String shift_type;
+    private String shift_timing;
+    private String fooding;
+    private String lodging;
+    private String other_benefits_for_employees;
+    private String in_hand_salary;
+    private String ctc;
+    private String currency;
+    private String minimum_salary_stipend;
+    private String maximum_salary_stipend;
+    private String overtime_per_day_hr;
+    private String ot_amount;
+    private String job_opening_status;
+    private String date_opened;
+    private String job_type;
+    private String is_hot_job_opening;
+    private String assigned_recruiter;
+    private String city;
+    private String state_province;
+    private String country;
+    private double postal_code;
+    private String region_of_work_location_in_india;
+    private double added_by;
+    private String created_on;
+    private double edited_by;
+    private String updated_on;
+    private String published;
+    private String revenue_per_position;
+    private double indsId;
+    private String inds_name;
+    private String inds_desc;
+    private String inds_logo;
+    private String inds_status;
+    private String created_at;
+    private double StCode;
+    private String StateName;
+
+
+    // Getter Methods
+
+    public double getId() {
+        return id;
+    }
+
+    public String getVacancy_master_id() {
+        return vacancy_master_id;
+    }
+
+    public String getNaps_opportunity_id() {
+        return naps_opportunity_id;
+    }
+
+    public String getZoho_recruit_id() {
+        return zoho_recruit_id;
+    }
+
+    public String getZoho_sourcing_id() {
+        return zoho_sourcing_id;
+    }
+
+    public String getVacancy_title() {
+        return vacancy_title;
+    }
+
+    public String getClient_name() {
+        return client_name;
+    }
+
+    public double getIndustry() {
+        return industry;
+    }
+
+    public String getContact_name() {
+        return contact_name;
+    }
+
+    public double getNumber_of_open_positions() {
+        return number_of_open_positions;
+    }
+
+    public double getYears_of_exp_required() {
+        return years_of_exp_required;
+    }
+
+    public double getMonths_of_exp_required() {
+        return months_of_exp_required;
+    }
+
+    public String getJob_responsibilities() {
+        return job_responsibilities;
+    }
+
+    public String getGender_preferences() {
+        return gender_preferences;
+    }
+
+    public String getJob_description() {
+        return job_description;
+    }
+
+    public String getSkills() {
+        return skills;
+    }
+
+    public String getLanguage_preference() {
+        return language_preference;
+    }
+
+    public String getMinimum_education() {
+        return minimum_education;
+    }
+
+    public String getOther_eligibility_criterea() {
+        return other_eligibility_criterea;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public String getShift_type() {
+        return shift_type;
+    }
+
+    public String getShift_timing() {
+        return shift_timing;
+    }
+
+    public String getFooding() {
+        return fooding;
+    }
+
+    public String getLodging() {
+        return lodging;
+    }
+
+    public String getOther_benefits_for_employees() {
+        return other_benefits_for_employees;
+    }
+
+    public String getIn_hand_salary() {
+        return in_hand_salary;
+    }
+
+    public String getCtc() {
+        return ctc;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public String getMinimum_salary_stipend() {
+        return minimum_salary_stipend;
+    }
+
+    public String getMaximum_salary_stipend() {
+        return maximum_salary_stipend;
+    }
+
+    public String getOvertime_per_day_hr() {
+        return overtime_per_day_hr;
+    }
+
+    public String getOt_amount() {
+        return ot_amount;
+    }
+
+    public String getJob_opening_status() {
+        return job_opening_status;
+    }
+
+    public String getDate_opened() {
+        return date_opened;
+    }
+
+    public String getJob_type() {
+        return job_type;
+    }
+
+    public String getIs_hot_job_opening() {
+        return is_hot_job_opening;
+    }
+
+    public String getAssigned_recruiter() {
+        return assigned_recruiter;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getState_province() {
+        return state_province;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public double getPostal_code() {
+        return postal_code;
+    }
+
+    public String getRegion_of_work_location_in_india() {
+        return region_of_work_location_in_india;
+    }
+
+    public double getAdded_by() {
+        return added_by;
+    }
+
+    public String getCreated_on() {
+        return created_on;
+    }
+
+    public double getEdited_by() {
+        return edited_by;
+    }
+
+    public String getUpdated_on() {
+        return updated_on;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public String getRevenue_per_position() {
+        return revenue_per_position;
+    }
+
+    public double getIndsId() {
+        return indsId;
+    }
+
+    public String getInds_name() {
+        return inds_name;
+    }
+
+    public String getInds_desc() {
+        return inds_desc;
+    }
+
+    public String getInds_logo() {
+        return inds_logo;
+    }
+
+    public String getInds_status() {
+        return inds_status;
+    }
+
+    public String getCreated_at() {
+        return created_at;
+    }
+
+    public double getStCode() {
+        return StCode;
+    }
+
+    public String getStateName() {
+        return StateName;
+    }
+
+    // Setter Methods
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setVacancy_master_id(String vacancy_master_id) {
+        this.vacancy_master_id = vacancy_master_id;
+    }
+
+    public void setNaps_opportunity_id(String naps_opportunity_id) {
+        this.naps_opportunity_id = naps_opportunity_id;
+    }
+
+    public void setZoho_recruit_id(String zoho_recruit_id) {
+        this.zoho_recruit_id = zoho_recruit_id;
+    }
+
+    public void setZoho_sourcing_id(String zoho_sourcing_id) {
+        this.zoho_sourcing_id = zoho_sourcing_id;
+    }
+
+    public void setVacancy_title(String vacancy_title) {
+        this.vacancy_title = vacancy_title;
+    }
+
+    public void setClient_name(String client_name) {
+        this.client_name = client_name;
+    }
+
+    public void setIndustry(double industry) {
+        this.industry = industry;
+    }
+
+    public void setContact_name(String contact_name) {
+        this.contact_name = contact_name;
+    }
+
+    public void setNumber_of_open_positions(double number_of_open_positions) {
+        this.number_of_open_positions = number_of_open_positions;
+    }
+
+    public void setYears_of_exp_required(double years_of_exp_required) {
+        this.years_of_exp_required = years_of_exp_required;
+    }
+
+    public void setMonths_of_exp_required(double months_of_exp_required) {
+        this.months_of_exp_required = months_of_exp_required;
+    }
+
+    public void setJob_responsibilities(String job_responsibilities) {
+        this.job_responsibilities = job_responsibilities;
+    }
+
+    public void setGender_preferences(String gender_preferences) {
+        this.gender_preferences = gender_preferences;
+    }
+
+    public void setJob_description(String job_description) {
+        this.job_description = job_description;
+    }
+
+    public void setSkills(String skills) {
+        this.skills = skills;
+    }
+
+    public void setLanguage_preference(String language_preference) {
+        this.language_preference = language_preference;
+    }
+
+    public void setMinimum_education(String minimum_education) {
+        this.minimum_education = minimum_education;
+    }
+
+    public void setOther_eligibility_criterea(String other_eligibility_criterea) {
+        this.other_eligibility_criterea = other_eligibility_criterea;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setShift_type(String shift_type) {
+        this.shift_type = shift_type;
+    }
+
+    public void setShift_timing(String shift_timing) {
+        this.shift_timing = shift_timing;
+    }
+
+    public void setFooding(String fooding) {
+        this.fooding = fooding;
+    }
+
+    public void setLodging(String lodging) {
+        this.lodging = lodging;
+    }
+
+    public void setOther_benefits_for_employees(String other_benefits_for_employees) {
+        this.other_benefits_for_employees = other_benefits_for_employees;
+    }
+
+    public void setIn_hand_salary(String in_hand_salary) {
+        this.in_hand_salary = in_hand_salary;
+    }
+
+    public void setCtc(String ctc) {
+        this.ctc = ctc;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public void setMinimum_salary_stipend(String minimum_salary_stipend) {
+        this.minimum_salary_stipend = minimum_salary_stipend;
+    }
+
+    public void setMaximum_salary_stipend(String maximum_salary_stipend) {
+        this.maximum_salary_stipend = maximum_salary_stipend;
+    }
+
+    public void setOvertime_per_day_hr(String overtime_per_day_hr) {
+        this.overtime_per_day_hr = overtime_per_day_hr;
+    }
+
+    public void setOt_amount(String ot_amount) {
+        this.ot_amount = ot_amount;
+    }
+
+    public void setJob_opening_status(String job_opening_status) {
+        this.job_opening_status = job_opening_status;
+    }
+
+    public void setDate_opened(String date_opened) {
+        this.date_opened = date_opened;
+    }
+
+    public void setJob_type(String job_type) {
+        this.job_type = job_type;
+    }
+
+    public void setIs_hot_job_opening(String is_hot_job_opening) {
+        this.is_hot_job_opening = is_hot_job_opening;
+    }
+
+    public void setAssigned_recruiter(String assigned_recruiter) {
+        this.assigned_recruiter = assigned_recruiter;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState_province(String state_province) {
+        this.state_province = state_province;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setPostal_code(double postal_code) {
+        this.postal_code = postal_code;
+    }
+
+    public void setRegion_of_work_location_in_india(String region_of_work_location_in_india) {
+        this.region_of_work_location_in_india = region_of_work_location_in_india;
+    }
+
+    public void setAdded_by(double added_by) {
+        this.added_by = added_by;
+    }
+
+    public void setCreated_on(String created_on) {
+        this.created_on = created_on;
+    }
+
+    public void setEdited_by(double edited_by) {
+        this.edited_by = edited_by;
+    }
+
+    public void setUpdated_on(String updated_on) {
+        this.updated_on = updated_on;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
+
+    public void setRevenue_per_position(String revenue_per_position) {
+        this.revenue_per_position = revenue_per_position;
+    }
+
+    public void setIndsId(double indsId) {
+        this.indsId = indsId;
+    }
+
+    public void setInds_name(String inds_name) {
+        this.inds_name = inds_name;
+    }
+
+    public void setInds_desc(String inds_desc) {
+        this.inds_desc = inds_desc;
+    }
+
+    public void setInds_logo(String inds_logo) {
+        this.inds_logo = inds_logo;
+    }
+
+    public void setInds_status(String inds_status) {
+        this.inds_status = inds_status;
+    }
+
+    public void setCreated_at(String created_at) {
+        this.created_at = created_at;
+    }
+
+    public void setStCode(double StCode) {
+        this.StCode = StCode;
+    }
+
+    public void setStateName(String StateName) {
+        this.StateName = StateName;
+    }
+}
